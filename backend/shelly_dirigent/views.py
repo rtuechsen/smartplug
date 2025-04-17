40899be4e8e389f3542444641d7b59e1af3c226e
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-04-17 20:39:17 +0200

    tried wrapping endpoints in a request manager

--- backend/shelly_dirigent/views.py
+++ backend/shelly_dirigent/views.py
@@ -1,25 +1,18 @@
-from django.http import JsonResponse
+
 from django.middleware.csrf import get_token
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
 
-@api_view(["GET"])
-def csrf(request):
-    """
-    Generates a CSRF token.
+class RequestManager:
 
-    Args:
-        request : The request for the CSRF token.
-
-    Returns:
-        JSON: The CSRF token.
-    """
-    return Response({"csrfToken": get_token(request)}, status=status.HTTP_200_OK)
+    @api_view(["GET"])
+    def csrf(self, request):
+        return Response({"csrfToken": get_token(request)}, status=status.HTTP_200_OK)
 
 
-@api_view(["POST", "GET"])  # Ping is being called with both methods.
-def ping(request):
-    return Response({"result": "worked!"}, status=status.HTTP_200_OK)
+    @api_view(["POST", "GET"])  # Ping is being called with both methods.
+    def ping(self, request):
+        return Response({"result": "worked!"}, status=status.HTTP_200_OK)
 
