efc164935425905ca8062618e078cb0edc8bbb67
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-04-17 16:48:18 +0200

    fixed settings path

--- backend/shelly_dirigent/wsgi.py
+++ backend/shelly_dirigent/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shelly_dirigent.settings.settings_production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shelly_dirigent.settings')
 
 application = get_wsgi_application()
