fc25c6a0c6820e07f1a7ff1dbe69ef3011c65e23
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-04-19 18:10:05 +0200

    added note about double API calls when using StrictMode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
+	// using StrictMode results in double API calls in development, this does not happen in production builds
 	<StrictMode>
 		<App />
 	</StrictMode>,
