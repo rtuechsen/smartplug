7d9444c9f03749820a78d4d332ed1fa66d443527
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-04-24 18:29:28 +0200

    fixed: allow to use venv when execution with sudo

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,7 @@ sudo systemctl start nginx
 
 cd backend
 
-gunicorn --bind 127.0.0.1:8000 shelly_dirigent.asgi:application --worker-class uvicorn.workers.UvicornWorker --workers 1
+sudo -E env PATH="$PATH" gunicorn --bind 127.0.0.1:8000 shelly_dirigent.asgi:application --worker-class uvicorn.workers.UvicornWorker --workers 1
 
 sudo systemctl restart gunicorn
 
