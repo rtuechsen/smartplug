f28194637b250914bc36cbdbf2f76e08fd89ae96
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-04-27 18:19:28 +0200

    added warnings to bash scripts that during migration runtime errors can occur

--- deploy.sh
+++ deploy.sh
@@ -21,9 +21,13 @@ cd ..
 
 cd backend
 
+printf "\n\n\x1B[33mStarting migration. During migration runtime errors can occure that do not happen at normal runtime.\x1B[0m\n\n\n"
+
 python3 ./manage.py makemigrations
 
 python3 ./manage.py migrate
+
+printf "\n\n\x1B[33mMigration finished. Runtime errors above can possibly be ignored if they do not show up below.\x1B[0m\n\n\n"
 
 cd ..
 
--- develop_backend.sh
+++ develop_backend.sh
@@ -5,9 +5,13 @@ cd backend
 
 sudo systemctl stop nginx
 
+printf "\n\n\x1B[33mStarting migration. During migration runtime errors can occure that do not happen at normal runtime.\x1B[0m\n\n\n"
+
 python3 manage.py makemigrations
 
 python3 manage.py migrate
+
+printf "\n\n\x1B[33mMigration finished. Runtime errors above can possibly be ignored if they do not show up below.\x1B[0m\n\n\n"
 
 sudo  -E env PATH="$PATH" uvicorn shelly_dirigent.asgi:application
 
