51408e6557cfb97be967b8aac99ee11ea12b6890
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-04-27 15:30:53 +0200

    added docstrings to settings

--- backend/shelly_dirigent/settings/__init__.py
+++ backend/shelly_dirigent/settings/__init__.py
@@ -1,10 +1,10 @@
+"""In order to use different settings for django in development and production builds we use this file to switch between two different configurations."""
 
 import os
 
-env = os.environ.get('DJANGO_PIPELINE', default='development')
+env = os.environ.get("DJANGO_PIPELINE", default="development")
 
-if env == 'production':
+if env == "production":
     from .settings_production import *
 else:
     from .settings_development import *
-    
--- backend/shelly_dirigent/settings/settings_production.py
+++ backend/shelly_dirigent/settings/settings_production.py
@@ -1,15 +1,15 @@
+"""Settings for production builds."""
 
-from .settings_base import *
 import os
+from .settings_base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# TODO
+
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # TODO
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'your-production-secret-key')
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "your-production-secret-key")
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
