8c59a18225e4f39f2094a49abf7d8ed0f9f0f12c
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-04-27 14:53:12 +0200

    added documentation, use BackendError and Logger

--- backend/shelly_dirigent/apps.py
+++ backend/shelly_dirigent/apps.py
@@ -1,18 +1,13 @@
 """@package apps
 Contains the TODO class which stores most of the data for the backend and also handles background tasks the REST API does not handle.
 
-This module is registered in the django settings as an app.
-- starts when server starts
-- reads and holds hierarchy of devices and groups as well as their current state
-- is used by the REST API to get or manipulate data
-- holds the mqtt client to communicate with the devices
+TODO: more details ???
 """
 
 import time
 import json
 import random
 import hashlib
-import warnings
 import threading
 from pathlib import Path
 from django.apps import AppConfig
@@ -27,39 +22,50 @@ from .TreeItem import TreeItem, TreeItemDevice, TreeItemGroup
 class InternalApp(AppConfig):
     """The main class for storing data about devices and groups as well as their state. Also handles background tasks the REST API does not handle.
 
-    TODO: more details
-    Apps in Django define and initialize usually all their attributes as class attributes.
-    Only initialization of attributes that require external django models happen inside ready().
+    This module is registered in the django settings as an app.
+    This means it is instanciated by django when the server starts.
+
+    It reads labor-config.json and holds the hierarchy of devices and groups as well as their current state.
+    It is used by the REST API to get or manipulate data from the device hierarchy.
+    It holds the mqtt client to communicate with the devices.
+
+    Functions that answer calls from the REST API should raise BackendError's.
+    Other function might do this as well if it makes sense.
+
+    Apps in Django usually define and initialize all their attributes as class attributes.
+    When Django has loaded all models the ready()-function of all apps are called.
     """
 
-    # TODO: consider renaming as well (has to match the folder !!!)
+    # TODO: consider renaming as well (has to match the folder!)
+    ## The name of the app (required by Django).
     name: str = "shelly_dirigent"
 
     ## Boolean needed to avoid starting background task multiple times.
     background_task_started: bool = False
 
-    ## main data structure to hold the data of the devices and groups
+    ## The main data structure to hold the hierarchy of devices and groups and their current state.
     device_tree: list[TreeItemDevice | TreeItemGroup]
 
-    ## mutex to avoid race conditions on the device tree
+    ## A mutex to avoid race conditions on the device tree. Needed because async calls from the REST API are possible. ALWAYS lock this mutex when reading or manipulating the device tree!
     device_tree_mutex: threading.Lock = threading.Lock()
 
-    ## mapping to get the TreeItem for a given id
+    ## A mapping to get the TreeItem for a given id.
     id_to_tree_item_mapping: dict[str, TreeItem] = {}
 
-    ## mapping to get the TreeItem for a given deviceId
+    ## A mapping to get the TreeItem for a given deviceId.
     device_id_to_tree_item_mapping: dict[str, TreeItem] = {}
 
-    ## The logger instance (singleton) to log events and errors
+    ## The logger instance (singleton) to log events and errors.
     logger: Logger = Logger()
 
     def ready(self):
 
-        InternalApp.logger.log("Server was started.")
+        InternalApp.logger.info("Server was started.")
 
         # load the labor-config.json
         self.load_labor_config()
 
+        # TODO: remove, used for debugging only
         if not InternalApp.background_task_started:
             InternalApp.background_task_started = True
             thread = threading.Thread(target=self.loop, daemon=True)
@@ -78,8 +84,14 @@ class InternalApp(AppConfig):
             )
 
     def load_labor_config(self) -> list[TreeItemDevice | TreeItemGroup]:
+        """Loads the hierarchy of devices and groups from `labor-config.json`.
 
-        # 1. read the file
+        The file 'labor-config.json' is expected to be located in the root directory of this project.
+
+        @return The hierarchy of devices and groups.
+        """
+
+        # 1. read the labor-config.json file
 
         labor_config_file_path: str = "./labor-config.json"
 
@@ -89,20 +101,27 @@ class InternalApp(AppConfig):
             with open(lab_config_path, "r", encoding="utf8") as file:
                 lab_config_json_string = file.read()
         except FileNotFoundError:
-            print(f"Error: Could not find the file {lab_config_path}")
+            self.logger.error(
+                f"Could not find the file labor-config at {lab_config_path}."
+            )
         except IOError:
-            print(f"Error: while reading the file {lab_config_path}")
+            self.logger.error(
+                f"Error while reading the file labor-config at {lab_config_path}."
+            )
 
-        # 2. convert to JSON (dicts and lists)
+        # 2. convert string from file to JSON (dicts and lists)
 
         try:
             lab_config_python_obj = json.loads(lab_config_json_string)
         except ValueError as e:
-            print(f"Error: Could not parse JSON {lab_config_json_string} because {e}")
+            self.logger.error(
+                f"Could not parse the labor-config to JSON because {e}: {lab_config_json_string}."
+            )
 
         # 3. convert to classes
 
         with InternalApp.device_tree_mutex:
+            # errors from parsing will not be logged but will result in an unhandled exception immediately after starting the server
             InternalApp.device_tree = self.object_list_to_tree_item_list(
                 lab_config_python_obj
             )
@@ -117,24 +136,37 @@ class InternalApp(AppConfig):
         )
 
     def object_list_to_tree_item_list(self, object_list: list[dict]) -> list[TreeItem]:
-        """Converts a list of dictionaries (JSON) to a list of TreeItems"""
+        """Converts a list of dictionaries (JSON) to a list of TreeItems.
 
-        # TODO: this causes doxygen to think self.object_to_tree_item is a new instance attribute ...
+        @param object_list A list of dictionaries representing tree items.
+
+        @return A list of TreeItems.
+        """
+
+        # Note: passing a member function as a callback causes doxygen to think it is a new attribute.
+        # Seems to be a bug fixed in doxygen 1.13 but that is not available to linux via apt.
         return list(map(self.object_to_tree_item, object_list))
 
     def object_to_tree_item(self, obj: dict) -> TreeItem:
-        """Converts a (hierarchy of) dictionary (JSON) to a (hierarchy of) TreeItem"""
+        """Converts a (hierarchy of) dictionary(s) (aka JSON) to a (hierarchy of) TreeItem(s).
+
+        Verifies the structure of the data and provides feedback.
+
+        @param obj A dictionary representing a tree item, possibly with more tree items as childrens.
+
+        @return A TreeItem with possibly more TreeItems as its children.
+        """
 
         # also verifies the correctness of the data, providing feedback to the admin using error messages
 
         if len(obj.keys()) != 2:
-            raise RuntimeError(f"Error: object {obj} has not exactly two keys!")
+            raise BackendError(f"Object {obj} has not exactly two properties.")
         if "label" not in obj.keys():
-            raise RuntimeError(f"Error: object {obj} is missing 'label'!")
+            raise BackendError(f"Object {obj} is missing property 'label'.")
 
         if "deviceId" in obj.keys():
             if obj["deviceId"] in self.device_id_to_tree_item_mapping:
-                raise RuntimeError(f"Error: deviceId of {obj} is not unique!")
+                raise BackendError(f"Property 'deviceId' of {obj} is not unique.")
             tree_item = TreeItemDevice()
             tree_item.deviceId = obj["deviceId"]
             tree_item.isOn = False
@@ -145,17 +177,17 @@ class InternalApp(AppConfig):
             tree_item = TreeItemGroup()
             tree_item.children = self.object_list_to_tree_item_list(obj["children"])
             if len(tree_item.children) == 0:
-                warnings.warn(f"Warning: object {obj} is a group without children!")
+                self.logger.warn(f"Object {obj} is a group without children.")
 
         else:
-            raise RuntimeError(
-                f"Error: object {obj} is missing both 'deviceId' and 'children'!"
+            raise BackendError(
+                f"Object {obj} is missing both 'deviceId' and 'children'."
             )
 
         tree_item.label = obj["label"]
 
-        # use (cryptographic) hash of label for id in order to keep the same id across runs
-        # this hides the deviceId of the shelly plugs from the clients and gives ids to groups as well
+        # Use (cryptographic) hash of the items label for the id in order to keep the same id across runs.
+        # This hides the deviceId of the shelly-plugs from the clients and gives ids to groups as well.
         hash_source: str = tree_item.label
         tree_item.id = hashlib.sha256(str.encode(hash_source)).hexdigest()
         while tree_item.id in InternalApp.id_to_tree_item_mapping:
@@ -189,7 +221,10 @@ class InternalApp(AppConfig):
                     tree_item.isOn = not tree_item.isOn
 
     def get_device_tree_dicts(self) -> list[dict]:
-        """Function to answer a call to /gettree, returns the current state of the tree."""
+        """Function to answer a call to /gettree, returns the current state of the tree.
+
+        @return A hierarchy of dictionaries and lists representing the current state of the device tree.
+        """
 
         device_tree_dict: list[dict] = []
 
@@ -205,12 +240,22 @@ class InternalApp(AppConfig):
         return device_tree_dict
 
     def switch(self, id: str, isOn: bool) -> None:
-        """Function to answer a call to /switch, turns groups and devices on/off according to the request."""
+        """Function to answer a call to /switch, turns devices and groups on/off according to the request.
+
+        @param id The id of the device or group to switch.
+
+        @param isOn A boolean indicating if the item should be turned on (True) or off (False). Ignores PEP8 naming convention to match the name of the variable across the project.
+        """
 
         def switch_recursive(id: str, isOn: bool):
+            """A helper function that switches the item as well as all children in case the item is a group.
+
+            @param id The id of the device or group to switch.
+
+            @param isOn A boolean indicating if the item should be turned on (True) or off (False). Ignores PEP8 naming convention to match the name of the variable across the project.
+            """
 
             if id not in InternalApp.id_to_tree_item_mapping:
-                # this error will automatically be propagated back as a proper response to the requesting client
 
                 raise BackendError(
                     f"Specified id {id} does not exist.",
