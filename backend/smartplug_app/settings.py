cf256e1bf378419c7339ad4ccfef4dda762730d8
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-05-02 09:50:37 +0200

    add settings for delays

---
+++ backend/smartplug_app/settings.py
@@ -0,0 +1,5 @@
+## A float value in seconds that determines how long to wait before switching a recently switched device again.
+SWITCHING_TOGGLE_DELAY = 5.0
+
+## A float value in seconds that determines how long to wait between switching on devices when switching on multiple devices.
+INRUSH_CURRENT_DELAY = 1.0
