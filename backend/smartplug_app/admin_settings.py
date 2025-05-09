fae3e793df4df3fdca989fa9b0d618f47067dab3
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-05-09 14:44:34 +0200

    use python docstring

--- backend/smartplug_app/admin_settings.py
+++ backend/smartplug_app/admin_settings.py
@@ -1,5 +1,9 @@
-## A float value in seconds that determines how long to wait before switching a recently switched device again.
-SWITCHING_TOGGLE_DELAY = 5.0
+"""TODO: module docstring"""
 
-## A float value in seconds that determines how long to wait between switching on devices when switching on multiple devices.
+SWITCHING_TOGGLE_DELAY = 5.0
+"""A float value in seconds that determines how long to wait before switching a
+recently switched device again."""
+
 INRUSH_CURRENT_DELAY = 1.0
+"""A float value in seconds that determines how long to wait between switching
+on devices when switching on multiple devices."""
