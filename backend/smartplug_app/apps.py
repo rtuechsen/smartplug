f7b56da6ba59292d4daa8fa261d2937be79cbe44
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-05-18 11:17:40 +0200

    removed 'labor' from 'labor-config'

--- backend/smartplug_app/apps.py
+++ backend/smartplug_app/apps.py
@@ -28,7 +28,7 @@ class SmartplugApp(AppConfig):
     This module is registered in the django settings as an app. This
     means it is instanciated by django when the server starts.
 
-    It reads labor-config.json and holds the hierarchy of devices and
+    It reads config.json and holds the hierarchy of devices and
     groups as well as their current state. It is used by the REST API to
     get or manipulate data from the device hierarchy. It holds the mqtt
     client to communicate with the devices.
@@ -70,8 +70,8 @@ class SmartplugApp(AppConfig):
 
         SmartplugApp._logger.info("Server was started.")
 
-        # load the labor-config.json
-        self._load_labor_config()
+        # load the config.json
+        self._load_config()
 
         # TODO: remove, used for debugging only
         # if not SmartplugApp._background_task_started:
@@ -91,21 +91,21 @@ class SmartplugApp(AppConfig):
                 "device_tree_update", "message", self.get_device_tree_dicts()
             )
 
-    def _load_labor_config(self) -> list[TreeItemDevice | TreeItemGroup]:
-        """Loads the hierarchy of devices and groups from `labor-config.json`.
+    def _load_config(self) -> list[TreeItemDevice | TreeItemGroup]:
+        """Loads the hierarchy of devices and groups from `config.json`.
 
-        The file 'labor-config.json' is expected to be located in the root
+        The file 'config.json' is expected to be located in the root
         directory of this project.
 
         @return The hierarchy of devices and groups.
         """
 
-        # 1. read the labor-config.json file
+        # 1. read the config.json file
 
-        labor_config_file_path: str = "./labor-config.json"
+        config_file_path: str = "./config.json"
 
         lab_config_path = (
-            Path(__file__).parent.parent.parent / labor_config_file_path
+            Path(__file__).parent.parent.parent / config_file_path
         )
 
         try:
@@ -113,11 +113,11 @@ class SmartplugApp(AppConfig):
                 lab_config_json_string = file.read()
         except FileNotFoundError:
             self._logger.error(
-                f"Could not find the file labor-config at {lab_config_path}."
+                f"Could not find the file config.json at {lab_config_path}."
             )
         except IOError:
             self._logger.error(
-                f"Error while reading the file labor-config at "
+                f"Error while reading the file config.json at "
                 f"{lab_config_path}."
             )
 
@@ -127,7 +127,7 @@ class SmartplugApp(AppConfig):
             lab_config_python_obj = json.loads(lab_config_json_string)
         except ValueError as e:
             self._logger.error(
-                "Could not parse the labor-config to JSON because "
+                "Could not parse config.json to JSON because "
                 f"{e}: {lab_config_json_string}."
             )
 
