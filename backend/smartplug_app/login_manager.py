12b72c726da7f99465c294c37ded18a4cef6d6c8
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-05-18 13:48:45 +0200

    reorder methods

--- backend/smartplug_app/login_manager.py
+++ backend/smartplug_app/login_manager.py
@@ -17,43 +17,9 @@ from .admin_settings import (
 
 
 class LoginManager:
+
     def __init__(self):
         pass
-
-    def _validate_request_origin(self, request: Request) -> None:
-        # Check that the values from the start of the session match
-        # with the values of this request.
-        try:
-            # This data has been set on login and will be checked when verifying
-            # the request origin.
-            user_agent = request.session["HTTP_USER_AGENT"]
-            accept_language = request.session["HTTP_ACCEPT_LANGUAGE"]
-            ip_address = request.session["REMOTE_ADDR"]
-
-            # Compare request header to origin of login.
-            if all(
-                [
-                    user_agent == request.META.get("HTTP_USER_AGENT"),
-                    accept_language
-                    == request.META.get("HTTP_ACCEPT_LANGUAGE"),
-                    ip_address == request.META.get("REMOTE_ADDR"),
-                ]
-            ):
-                return
-
-            raise BackendError(
-                message=f"Request origin mismatch on user: {request.session['USERNAME']}.",
-                status_code=status.HTTP_401_UNAUTHORIZED,
-            )
-
-        # KeyError occurs when the request is missing necessary data for
-        # verification.
-        except KeyError as e:
-            raise BackendError(
-                message="A request has been made by a user who is not signed in.",
-                status_code=status.HTTP_400_BAD_REQUEST,
-                user_message="Authentication failed. Are you signed in?",
-            ) from e
 
     def login(self, request: Request) -> None:
         username = request.data.get("username")
@@ -79,6 +45,31 @@ class LoginManager:
         # from session fixation:
         # https://docs.djangoproject.com/en/5.2/topics/http/sessions/
         request.session.flush()
+
+    def get_user_permission(self, request: Request) -> None:
+        # user will be None unless logged in. Per default we use a
+        # database-backed session management. The session data is
+        # stored server-side and referenced by the session-id.
+        # https://stackoverflow.com/questions/5113421/what-is-the-difference-between-a-cookie-and-a-session-in-django
+        # https://docs.djangoproject.com/en/5.2/topics/http/sessions/
+
+        # We validate the origin of the request. _validate_request_origin()
+        # will throw an exception if anything is wrong.
+        self._validate_request_origin(request)
+
+        try:
+            user = request.session.get("USERNAME")
+        except KeyError as e:
+            raise BackendError(
+                message="A request has been made by a user who is not signed in.",
+                status_code=status.HTTP_400_BAD_REQUEST,
+                user_message="Authentication failed. Are you signed in?",
+            ) from e
+
+        if user:
+            # If there is a user and the user needs permission, it means an action happened.
+            # We therefor reset the expiry using the value in our settings.
+            request.session.set_expiry(settings.SESSION_COOKIE_AGE)
 
     def _authenticate(self, username: str, password: str) -> tuple[str, str]:
         """Verifies that the combination of username and passowrd belongs to
@@ -171,27 +162,37 @@ class LoginManager:
                 "Please contact the admin.",
             ) from e
 
-    def get_user_permission(self, request: Request) -> None:
-        # user will be None unless logged in. Per default we use a
-        # database-backed session management. The session data is
-        # stored server-side and referenced by the session-id.
-        # https://stackoverflow.com/questions/5113421/what-is-the-difference-between-a-cookie-and-a-session-in-django
-        # https://docs.djangoproject.com/en/5.2/topics/http/sessions/
-
-        # We validate the origin of the request. _validate_request_origin()
-        # will throw an exception if anything is wrong.
-        self._validate_request_origin(request)
-
+    def _validate_request_origin(self, request: Request) -> None:
+        # Check that the values from the start of the session match
+        # with the values of this request.
         try:
-            user = request.session.get("USERNAME")
+            # This data has been set on login and will be checked when verifying
+            # the request origin.
+            user_agent = request.session["HTTP_USER_AGENT"]
+            accept_language = request.session["HTTP_ACCEPT_LANGUAGE"]
+            ip_address = request.session["REMOTE_ADDR"]
+
+            # Compare request header to origin of login.
+            if all(
+                [
+                    user_agent == request.META.get("HTTP_USER_AGENT"),
+                    accept_language
+                    == request.META.get("HTTP_ACCEPT_LANGUAGE"),
+                    ip_address == request.META.get("REMOTE_ADDR"),
+                ]
+            ):
+                return
+
+            raise BackendError(
+                message=f"Request origin mismatch on user: {request.session['USERNAME']}.",
+                status_code=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        # KeyError occurs when the request is missing necessary data for
+        # verification.
         except KeyError as e:
             raise BackendError(
                 message="A request has been made by a user who is not signed in.",
                 status_code=status.HTTP_400_BAD_REQUEST,
                 user_message="Authentication failed. Are you signed in?",
             ) from e
-
-        if user:
-            # If there is a user and the user needs permission, it means an action happened.
-            # We therefor reset the expiry using the value in our settings.
-            request.session.set_expiry(settings.SESSION_COOKIE_AGE)
