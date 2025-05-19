ab12d2fe87fdec7c37d0fd49f444e1279470e9ec
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-05-19 18:21:20 +0200

    generate base dn for ldap search based on user name

--- backend/smartplug_app/admin_settings.py
+++ backend/smartplug_app/admin_settings.py
@@ -1,8 +1,11 @@
+"""TODO"""
+
 # TODO: store settings here or in settings_base.py ???
 
 # TODO: remove, development variable
 USE_LDAP: bool = False
 
+# TODO: use correct host name
 LDAP_SERVER_ADDRESS_AND_PORT: str = "ldap://192.168.178.50:389"
 """The address of the server running active directory. It should start with 
 'ldap://' and end with ':389' (the port for LDAP).
@@ -11,10 +14,4 @@ LDAP_SERVER_ADDRESS_AND_PORT: str = "ldap://192.168.178.50:389"
 LDAP_TIMEOUT_SECONDS: int = 5
 """The time in seconds after which a request to the active directory server is 
 considered a timeout.
-"""
-
-LDAP_SEARCH_BASE_DN = "dc=mylab,dc=local"
-"""The search path when looking up information about the users. If the 
-User Principle Name was john.doe@mylab.local the LDAP_SEARCH_BASE_DN would be
-'dc=mylab,dc=local'.
 """
