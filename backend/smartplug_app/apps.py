b3d3474772355819aba7b70054efb3c3bf99fc89
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-05-20 17:54:23 +0200

    format

--- backend/smartplug_app/apps.py
+++ backend/smartplug_app/apps.py
@@ -139,9 +139,7 @@ class SmartplugApp(AppConfig):
 
         config_file_path: str = "./config.json"
 
-        lab_config_path = (
-            Path(__file__).parent.parent.parent / config_file_path
-        )
+        lab_config_path = Path(__file__).parent.parent.parent / config_file_path
 
         try:
             with open(lab_config_path, "r", encoding="utf8") as file:
@@ -152,8 +150,7 @@ class SmartplugApp(AppConfig):
             )
         except IOError:
             self._logger.error(
-                f"Error while reading the file config.json at "
-                f"{lab_config_path}."
+                f"Error while reading the file config.json at " f"{lab_config_path}."
             )
 
         # 2. convert string from file to JSON (dicts and lists)
@@ -184,9 +181,7 @@ class SmartplugApp(AppConfig):
             len(SmartplugApp._device_id_to_tree_item_mapping.keys()) * 2
         )
 
-    def _object_list_to_tree_item_list(
-        self, object_list: list[dict]
-    ) -> list[TreeItem]:
+    def _object_list_to_tree_item_list(self, object_list: list[dict]) -> list[TreeItem]:
         """Converts a list of dictionaries (JSON) to a list of TreeItems.
 
         @param object_list A list of dictionaries representing tree items.
@@ -222,22 +217,16 @@ class SmartplugApp(AppConfig):
 
         if "deviceId" in obj.keys():
             if obj["deviceId"] in self._device_id_to_tree_item_mapping:
-                raise BackendError(
-                    f"Property 'deviceId' of {obj} is not unique."
-                )
+                raise BackendError(f"Property 'deviceId' of {obj} is not unique.")
             tree_item = TreeItemDevice()
             tree_item.deviceId = obj["deviceId"]
             tree_item.isOn = False
             tree_item.isAvailable = False
-            self._device_id_to_tree_item_mapping[tree_item.deviceId] = (
-                tree_item
-            )
+            self._device_id_to_tree_item_mapping[tree_item.deviceId] = tree_item
 
         elif "children" in obj.keys():
             tree_item = TreeItemGroup()
-            tree_item.children = self._object_list_to_tree_item_list(
-                obj["children"]
-            )
+            tree_item.children = self._object_list_to_tree_item_list(obj["children"])
             if len(tree_item.children) == 0:
                 self._logger.warn(f"Object {obj} is a group without children.")
 
@@ -272,9 +261,9 @@ class SmartplugApp(AppConfig):
             device_id: str = random.choice(
                 list(self._device_id_to_tree_item_mapping.keys())
             )
-            tree_item: TreeItemDevice = (
-                SmartplugApp._device_id_to_tree_item_mapping[device_id]
-            )
+            tree_item: TreeItemDevice = SmartplugApp._device_id_to_tree_item_mapping[
+                device_id
+            ]
 
             toggle_availability: bool = random.choice([True, False])
 
--- backend/smartplug_app/login_manager.py
+++ backend/smartplug_app/login_manager.py
@@ -8,57 +8,71 @@ from rest_framework import status
 
 
 def authenticate(username: str, password: str) -> None:
-    if username == 'user' and password == 'pass':
+    if username == "user" and password == "pass":
         return
 
-    raise BackendError(message=f'Credentials mismatch on user: {username}.',
-                       status_code=status.HTTP_401_UNAUTHORIZED,
-                       user_message='Either your password or username were incorrect.')
+    raise BackendError(
+        message=f"Credentials mismatch on user: {username}.",
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        user_message="Either your password or username were incorrect.",
+    )
 
 
 class LoginManager:
     def __init__(self):
         pass
 
-    def _validate_request_origin(request: Request) -> None:
+    def _validate_request_origin(self, request: Request) -> None:
         # Check that the values from the start of the session match
         # with the values of this request.
         try:
-            # This data has been set on login and will be checked when verifying
-            # the request origin.
-            user_agent = request.session['HTTP_USER_AGENT']
-            accept_language = request.session['HTTP_ACCEPT_LANGUAGE']
-            ip_address = request.session['REMOTE_ADDR']
+            # This data has been set on login and will be checked when
+            # verifying the request origin.
+            user_agent = request.session["HTTP_USER_AGENT"]
+            accept_language = request.session["HTTP_ACCEPT_LANGUAGE"]
+            ip_address = request.session["REMOTE_ADDR"]
 
             # Compare request header to origin of login.
-            if all([
-                user_agent == request.META.get('HTTP_USER_AGENT'),
-                accept_language == request.META.get('HTTP_ACCEPT_LANGUAGE'),
-                ip_address == request.META.get('REMOTE_ADDR'),
-            ]):
+            if all(
+                [
+                    user_agent == request.META.get("HTTP_USER_AGENT"),
+                    accept_language
+                    == request.META.get("HTTP_ACCEPT_LANGUAGE"),
+                    ip_address == request.META.get("REMOTE_ADDR"),
+                ]
+            ):
                 return
 
-            raise BackendError(message=f'Request origin mismatch on user: {request.session['USERNAME']}.',
-                               status_code=status.HTTP_401_UNAUTHORIZED)
+            raise BackendError(
+                message=f"Request origin mismatch on user: "
+                f"{request.session['USERNAME']}.",
+                status_code=status.HTTP_401_UNAUTHORIZED,
+            )
 
         # KeyError occurs when the request is missing necessary data for
         # verification.
-        except KeyError:
-            raise BackendError(message=f'A request has been made by a user who is not signed in.',
-                               status_code=status.HTTP_400_BAD_REQUEST,
-                               user_message="Authentication failed. Are you signed in?")
+        except KeyError as e:
+            raise BackendError(
+                message="A request has been made by a user who is not signed "
+                "in.",
+                status_code=status.HTTP_400_BAD_REQUEST,
+                user_message="Authentication failed. Are you signed in?",
+            ) from e
 
         # TODO: Integrate LDAP into login logic
+
     def login(self, request: Request) -> None:
-        username = request.data.get('username')
-        password = request.data.get('password')
+        username = request.data.get("username")
+        password = request.data.get("password")
 
         authenticate(username=username, password=password)
 
-        request.session['USERNAME'] = username
-        request.session['HTTP_USER_AGENT'] = request.META['HTTP_USER_AGENT']
-        request.session['HTTP_ACCEPT_LANGUAGE'] = request.META['HTTP_ACCEPT_LANGUAGE']
-        request.session['REMOTE_ADDR'] = request.META['REMOTE_ADDR']
+        request.session["USERNAME"] = username
+        request.session["HTTP_USER_AGENT"] = request.META["HTTP_USER_AGENT"]
+        request.session["HTTP_ACCEPT_LANGUAGE"] = request.META[
+            "HTTP_ACCEPT_LANGUAGE"
+        ]
+        request.session["REMOTE_ADDR"] = request.META["REMOTE_ADDR"]
 
     def logout(self, request: Request) -> None:
         # Flushing the session will delete it and protects
@@ -78,14 +92,16 @@ class LoginManager:
         self._validate_request_origin(request)
 
         try:
-            user = request.session.get('USERNAME')
-        except KeyError:
+            user = request.session.get("USERNAME")
+        except KeyError as e:
             raise BackendError(
-                message=f'A request has been made by a user who is not signed in.',
+                message="A request has been made by a user who is not signed in.",
                 status_code=status.HTTP_400_BAD_REQUEST,
-                user_message="Authentication failed. Are you signed in?")
+                user_message="Authentication failed. Are you signed in?",
+            ) from e
 
         if user:
-            # If there is a user and the user needs permission, it means an action happened.
+            # If there is a user and the user needs permission, it means an
+            # action happened.
             # We therefor reset the expiry using the value in our settings.
             request.session.set_expiry(settings.SESSION_COOKIE_AGE)
