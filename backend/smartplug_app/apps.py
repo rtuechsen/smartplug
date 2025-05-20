2e480ec0b45df9536d9394173d5bb1812815156d
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-05-20 18:11:22 +0200

    enable background thread to sends random user list

--- backend/smartplug_app/apps.py
+++ backend/smartplug_app/apps.py
@@ -74,10 +74,10 @@ class SmartplugApp(AppConfig):
         self._load_config()
 
         # TODO: remove, used for debugging only
-        # if not SmartplugApp._background_task_started:
-        #     SmartplugApp._background_task_started = True
-        #     thread = threading.Thread(target=self.loop, daemon=True)
-        #     thread.start()
+        if not SmartplugApp._background_task_started:
+            SmartplugApp._background_task_started = True
+            thread = threading.Thread(target=self.loop, daemon=True)
+            thread.start()
 
     # TODO: remove, used for debugging only
     def loop(self) -> None:
@@ -115,8 +115,7 @@ class SmartplugApp(AppConfig):
             "Dmitri Mendeleev",
         ]
         while True:
-            time.sleep(4)
-            print("\n\n -> Running background task ...\n\n")
+            time.sleep(3)
             current_users = random.sample(users, random.randint(5, 15))
             # TODO: remove, used for debugging only
             # self.change_device_tree_randomly(10)
@@ -139,7 +138,9 @@ class SmartplugApp(AppConfig):
 
         config_file_path: str = "./config.json"
 
-        lab_config_path = Path(__file__).parent.parent.parent / config_file_path
+        lab_config_path = (
+            Path(__file__).parent.parent.parent / config_file_path
+        )
 
         try:
             with open(lab_config_path, "r", encoding="utf8") as file:
@@ -150,7 +151,8 @@ class SmartplugApp(AppConfig):
             )
         except IOError:
             self._logger.error(
-                f"Error while reading the file config.json at " f"{lab_config_path}."
+                f"Error while reading the file config.json at "
+                f"{lab_config_path}."
             )
 
         # 2. convert string from file to JSON (dicts and lists)
@@ -181,7 +183,9 @@ class SmartplugApp(AppConfig):
             len(SmartplugApp._device_id_to_tree_item_mapping.keys()) * 2
         )
 
-    def _object_list_to_tree_item_list(self, object_list: list[dict]) -> list[TreeItem]:
+    def _object_list_to_tree_item_list(
+        self, object_list: list[dict]
+    ) -> list[TreeItem]:
         """Converts a list of dictionaries (JSON) to a list of TreeItems.
 
         @param object_list A list of dictionaries representing tree items.
@@ -217,16 +221,22 @@ class SmartplugApp(AppConfig):
 
         if "deviceId" in obj.keys():
             if obj["deviceId"] in self._device_id_to_tree_item_mapping:
-                raise BackendError(f"Property 'deviceId' of {obj} is not unique.")
+                raise BackendError(
+                    f"Property 'deviceId' of {obj} is not unique."
+                )
             tree_item = TreeItemDevice()
             tree_item.deviceId = obj["deviceId"]
             tree_item.isOn = False
             tree_item.isAvailable = False
-            self._device_id_to_tree_item_mapping[tree_item.deviceId] = tree_item
+            self._device_id_to_tree_item_mapping[tree_item.deviceId] = (
+                tree_item
+            )
 
         elif "children" in obj.keys():
             tree_item = TreeItemGroup()
-            tree_item.children = self._object_list_to_tree_item_list(obj["children"])
+            tree_item.children = self._object_list_to_tree_item_list(
+                obj["children"]
+            )
             if len(tree_item.children) == 0:
                 self._logger.warn(f"Object {obj} is a group without children.")
 
@@ -261,9 +271,9 @@ class SmartplugApp(AppConfig):
             device_id: str = random.choice(
                 list(self._device_id_to_tree_item_mapping.keys())
             )
-            tree_item: TreeItemDevice = SmartplugApp._device_id_to_tree_item_mapping[
-                device_id
-            ]
+            tree_item: TreeItemDevice = (
+                SmartplugApp._device_id_to_tree_item_mapping[device_id]
+            )
 
             toggle_availability: bool = random.choice([True, False])
 
