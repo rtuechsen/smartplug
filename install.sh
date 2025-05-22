6a1bf928490e5e4943d4a22155cfffa93bf418b6
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-05-22 11:38:15 +0200

    added comment

--- install.sh
+++ install.sh
@@ -33,6 +33,7 @@ sudo cp -p ./smartplug_app /etc/logrotate.d/smartplug_app
 
 ## LDAP
 
+# TODO: what kind of password want slapd here? what is it for?
 sudo apt install -y libsasl2-dev python3-dev libldap2-dev libssl-dev slapd ldap-utils
 
 ## linter
