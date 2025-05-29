82eb85f8fdd839a5bf8c693d37fdcec68bbc1efb
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-05-29 10:21:07 +0200

    remove debug code

--- backend/smartplug_app/admin_settings.py
+++ backend/smartplug_app/admin_settings.py
@@ -6,7 +6,7 @@
 USE_SWITCHING_DELAYS: bool = True
 
 # TODO: add unit (seconds) to time variables
-SWITCHING_TOGGLE_DELAY: float = 120.0
+SWITCHING_TOGGLE_DELAY: float = 1.0
 """A float value in seconds that determines how long to wait before switching a
 recently switched device again."""
 
