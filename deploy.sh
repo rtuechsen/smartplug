a5fd01fb07d4bdbc35a897c82aa38e904d879cbc
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-05-29 12:56:04 +0200

    fix file reading issues during migration

--- deploy.sh
+++ deploy.sh
@@ -27,16 +27,17 @@ cd ..
 
 cd backend
 
-printf "\n\n\x1B[33mStarting migration. During migration runtime errors can occure that do not happen at normal runtime.\x1B[0m\n\n\n"
+printf "\n\n\x1B[33mStarting migration.\x1B[0m\n\n\n"
 
-python manage.py clearsessions
+sudo -E env PATH="$PATH" python manage.py clearsessions
 
-python3 ./manage.py makemigrations
+sudo -E env PATH="$PATH" python manage.py makemigrations
 
-python3 ./manage.py migrate
+sudo -E env PATH="$PATH" python manage.py migrate
 
-printf "\n\n\x1B[33mMigration finished. Runtime errors above can possibly be ignored if they do not show up below.\x1B[0m\n\n\n"
+printf "\n\n\x1B[33mMigration finished.\x1B[0m\n\n\n"
 
+# TODO: dont run pylint on server start ???
 pylint --recursive=y smartplug_app
 
 cd ..
