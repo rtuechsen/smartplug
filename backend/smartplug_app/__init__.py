ac89b639f58bcdddecfcb3fd31ca3f978c2c37a9
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-06-01 14:45:31 +0200

    add placeholder for high level documentation of backend

---
+++ backend/smartplug_app/__init__.py
@@ -0,0 +1,8 @@
+## \mainpage
+##
+## # Title
+## ## todo
+## - \ needed i docstrings if more than one sentence is used
+## - attributes need to be documented differently than other stuff
+## - mutex: Needed because async calls from the REST API are possible.
+## - id/isOn: ignore linter to have the same names throughout the project
