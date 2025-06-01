8d5773272bd61cef9d7b04f4449765496c996b7b
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-06-01 15:37:49 +0200

    improved docstrings

--- backend/smartplug_app/__init__.py
+++ backend/smartplug_app/__init__.py
@@ -6,3 +6,4 @@
 ## - attributes need to be documented differently than other stuff
 ## - mutex: Needed because async calls from the REST API are possible.
 ## - id/isOn: ignore linter to have the same names throughout the project
+## - LDAP TLS code is commented out
--- backend/smartplug_app/authentication_backend.py
+++ backend/smartplug_app/authentication_backend.py
@@ -1,3 +1,5 @@
+"""Contains a backend for authenticating incoming requests."""
+
 import ldap
 from django.contrib.auth.backends import BaseBackend
 from django.contrib.auth.models import User
@@ -14,29 +16,46 @@ from .admin_settings import (
 
 
 class AuthenticationBackend(BaseBackend):
-    """TODO"""
+    """A backend class to integrate custom authentication using LDAP into
+    Django.
+    """
 
     def authenticate(
         self, request: Request, username: str = None, password: str = None
     ) -> User:
+        """Function to authenticate a given request using username and
+        password.
+
+        This function is inherited from BaseBackend and required to be
+        implemented.
+
+        @param request The request that requires authentication.
+
+        @param username The username of the user making the request.
+
+        @param password The password of the user making the request.
+
+        @return The user object corresponding to the username and password.
+        """
 
         first_name, last_name = self._authenticate_ldap(username, password)
 
         try:
             user = User.objects.get(username=username)
         except User.DoesNotExist:
+            # If the user does not yet exist in Djangos database a new entry is
+            # created.
             user = User(
                 username=username, first_name=first_name, last_name=last_name
             )
             user.save()
 
-        # store data about the user in the session
         request.session["USERNAME"] = username
-        # TODO: first and last name are already stored in the user: remove them
-        # from the session ???
         request.session["FIRSTNAME"] = first_name
         request.session["LASTNAME"] = last_name
 
+        # We store some data about the user in the session. This is later used
+        # to detect cookie theft.
         request.session["HTTP_USER_AGENT"] = request.META["HTTP_USER_AGENT"]
         request.session["HTTP_ACCEPT_LANGUAGE"] = request.META[
             "HTTP_ACCEPT_LANGUAGE"
@@ -45,7 +64,16 @@ class AuthenticationBackend(BaseBackend):
 
         return user
 
-    def get_user(self, user_id):
+    def get_user(self, user_id: int) -> User | None:
+        """Function to get the corresponding user object given a user id.
+
+        This function is inherited from BaseBackend and required to be
+        implemented.
+
+        @param The id of the user.
+
+        @return The user object corresponding to the given id or None.
+        """
         try:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
@@ -54,18 +82,18 @@ class AuthenticationBackend(BaseBackend):
     def _authenticate_ldap(
         self, username: str, password: str
     ) -> tuple[str, str]:
-        """Verifies that the combination of username and passowrd belongs to a
-        user in the Active Directory.
+        """Function to verify that the combination of username and password
+        belongs to a user in the configured Active Directory.
 
-        Tries to retrieve the first and last name of the user from the Active
-        Directory as well.
+        It tries to retrieve the first and last name of the user from the
+        configured Active Directory as well.
 
         @param username The username to verify.
 
         @param password The password to verify.
 
         @return A tuple with (first name, last name) of the user retrieved from
-        the Active Directory.
+        the configured Active Directory.
         """
         # TODO: remove, development code
         if not USE_LDAP:
@@ -82,7 +110,8 @@ class AuthenticationBackend(BaseBackend):
                 raise BackendError(
                     message=f"Credentials mismatch on user: {username}.",
                     status_code=status.HTTP_401_UNAUTHORIZED,
-                    user_message="Either your password or username were incorrect.",
+                    user_message="Either your password or username were"
+                    "incorrect.",
                 )
 
         # TODO: need to get either logon name or UPN from ldap, use the same
@@ -96,32 +125,31 @@ class AuthenticationBackend(BaseBackend):
             # passwords
             conn.set_option(ldap.OPT_DEBUG_LEVEL, 255)
 
-            # LDAP 3 is necessary for active directory
+            # LDAP 3 is necessary for active directory.
             conn.set_option(ldap.OPT_PROTOCOL_VERSION, ldap.VERSION3)
 
             conn.set_option(ldap.OPT_NETWORK_TIMEOUT, LDAP_TIMEOUT_SECONDS)
 
-            # Important for AD: disable referrals
+            # Important for AD: disable referrals.
             conn.set_option(ldap.OPT_REFERRALS, 0)
 
-            # TODO: explain TLS code is commented out, mention in docs
+            # Commands required to enable TLS.
             # https://www.python-ldap.org/en/python-ldap-3.4.3/reference/ldap.html?highlight=tls#tls-options
             # conn.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_DEMAND)
-            # conn.set_option(ldap.OPT_X_TLS_CACERTFILE, "./cacert.pem")      # get/set path to PEM file with CA certs
+            # conn.set_option(ldap.OPT_X_TLS_CACERTFILE, "./cacert.pem")
             # conn.set_option(ldap.OPT_X_TLS_NEWCTX, 0)
             # conn.start_tls_s()
 
-            # The bind performs the actual request to verify the credentials
+            # The bind performs the actual request to verify the credentials.
             conn.simple_bind_s(username, password)
-            print("Successful bind.")
 
             # next get first and last name of the user (if those exist)
 
             if "@" in username:
                 # UPN = User Principle Name
-                # When searching the UPN equals the full email address of the
-                # user
-                search_filter = f"(userPrincipalName={username})"
+                # For searching, the UPN equals the full email address of the
+                # user.
+                search_filter: str = f"(userPrincipalName={username})"
 
                 domain_name: str = username.split("@")[1]
                 base_dn: str = ",".join(
@@ -141,7 +169,8 @@ class AuthenticationBackend(BaseBackend):
                 _, entry = result[0]
 
                 # Note: Active Directory apparently requires either the first
-                # name or the last name when creating a user
+                # name or the last name when creating a user. So either of them
+                # will be set.
 
                 if "givenName" in entry:
                     first_name = entry["givenName"][0].decode("UTF-8")
@@ -154,9 +183,9 @@ class AuthenticationBackend(BaseBackend):
                     last_name = ""
 
             else:
-                # Note: When using NetBIOS the base dn the credentials belong
-                # to cannot be deduced -> simply display the username in the
-                # frontend
+                # Note: When using NetBIOS, the base dn that the credentials
+                # belong to cannot be deduced -> simply display the username in
+                # the frontend.
                 first_name = username.split("\\")[1]
                 last_name = ""
 
