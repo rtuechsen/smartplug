31619d3466c6ea148e95279d41b1e242ad7cc441
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-06-05 08:29:15 +0200

    change admin settings

--- backend/smartplug_app/admin_settings.py
+++ backend/smartplug_app/admin_settings.py
@@ -15,7 +15,7 @@ INRUSH_CURRENT_DELAY: float = 0.5
 on devices when switching on multiple devices."""
 
 # TODO: remove, development variable
-USE_LDAP: bool = True
+USE_LDAP: bool = False
 
 # TODO: use correct host name
 LDAP_SERVER_ADDRESS_AND_PORT: str = "ldap://192.168.133.42:389"
@@ -30,4 +30,4 @@ LDAP_TIMEOUT_SECONDS: int = 5
 considered a timeout."""
 
 # TODO remove, development variable
-USE_MQTT: bool = True
+USE_MQTT: bool = False
