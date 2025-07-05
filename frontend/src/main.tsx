8d9bb375e7f607df17d88f35f8ec5ee922312581
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-07-05 15:32:02 +0200

    added docstrings

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,9 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
 
 
+/**
+ * The main call in React to create the root of the document.
+ */
 createRoot(document.getElementById('root')!).render(
 	// using StrictMode results in components being mounted multiple times (and API called multiple times)
 	// to detect side effects.
