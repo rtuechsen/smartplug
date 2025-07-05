886fd5a85dce3b8b58ae9c87c5a24f693cedff10
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-07-05 15:18:44 +0200

    added docstrings

--- frontend/src/LoadingButtonGroup.tsx
+++ frontend/src/LoadingButtonGroup.tsx
@@ -7,24 +7,40 @@ import { JSX } from '@emotion/react/jsx-runtime';
 
 
 /**
- * A data structure to pass information to an LoadingButton.  
+ * A data structure to pass information to an LoadingButton.
  */
 export interface LoadingButtonProps {
 
 	/**
-	 * The (async) callback used when clicking the button. 
+	 * The (async) callback used when clicking the button.
 	 * 
 	 * @returns A void Promise that can be waited on if one wants to wait for the function to complete.
 	 */
 	onClick: () => Promise<void>;
+
+	/**
+	 * The style of the used Button component (Material UI).
+	 * 
+	 * Can be one of: 'text', 'outlined', 'contained'
+	 */
 	variant?: OverridableStringUnion<'text' | 'outlined' | 'contained', ButtonPropsVariantOverrides>;
+
+	/**
+	 * An object of optional css stylings options to pass to the button component.
+	 */
 	sx?: object;
+
+	/**
+	 * A Ref used to reference the button from outside the LoadingButton component.
+	 * 
+	 * Used to trigger the button using the keyboard.
+	 */
 	ref?: React.Ref<HTMLButtonElement>;
 }
 
 
 /**
- * A button that shows a loading circle while waiting for its callback to finish.  
+ * A button that shows a loading circle while waiting for its callback to finish.
  * 
  * @param props The properties needed to construct the button.
  * 
@@ -61,19 +77,38 @@ export function LoadingButton({ onClick, variant, children, sx, ref }: React.Pro
 
 
 /**
- * A data structure to pass information to an LoadingButtonGroup.  
+ * A data structure to pass information to an LoadingButtonGroup.
  */
 export interface LoadingButtonGroupProps {
 
+	/**
+	 * The (async) callback to use for the left button.
+	 * 
+	 * @returns A void Promise that can be waited on if one wants to wait for the function to complete.
+	 */
 	onClickLeftButton: () => Promise<void>;
+
+	/**
+	 * The (async) callback to use for the right button.
+	 * 
+	 * @returns A void Promise that can be waited on if one wants to wait for the function to complete.
+	 */
 	onClickRightButton: () => Promise<void>;
+
+	/**
+	 * The label for the left button.
+	 */
 	buttonLabelLeft: string;
+
+	/**
+	 * The label for the right button.
+	 */
 	buttonLabelRight: string;
 }
 
 
 /**
- * A combined group of 'ON' and 'OFF' button which each show a loading circle while waiting for its callback to finish.
+ * A combined group of left and right button which each show a loading circle while waiting for its callback to finish.
  *
  * @param props Holds the data needed to construct the button.
  * 
