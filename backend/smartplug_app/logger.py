db11808a1ddcb1360d97cbd34a2f194735e3afc3
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-07-05 13:02:29 +0200

    add missing docstring, access class members using the class name instead of self

--- backend/smartplug_app/logger.py
+++ backend/smartplug_app/logger.py
@@ -38,13 +38,15 @@ class Logger:
     ## The (only) instance of this class.
     _instance: "Logger" = None
 
-    ## A thread safe queue that stores the logs.
-    _log_queue: Queue
+    ## A thread safe queue that stores the logs. The size of the queue is set
+    ## arbitrarily to 100.
+    _log_queue: Queue = Queue(maxsize=100)
 
     ## The output folder of log files. Set fixed to
     ## '/var/log/smartplug_app/'.
     _output_folder: Path = Path("/var/log/smartplug_app/")
 
+    ## The path of the active log file.
     _log_file_path: Path = _output_folder / "smartplug_app.log"
 
     def __new__(cls):
@@ -59,9 +61,6 @@ class Logger:
 
             # We need to do the initializations here because __init__() would
             # be called every time an instance is requested.
-
-            # The size of the queue is set arbitrarily to 100.
-            cls._instance._log_queue = Queue(maxsize=100)
 
             # check if outfolder exists and create it otherwise
             if not cls._instance._output_folder.is_dir():
@@ -177,7 +176,7 @@ class Logger:
         log.date = now.strftime("%Y-%m-%d")
         log.time = now.strftime("%H:%M:%S.%f")
 
-        self._log_queue.put(log)
+        Logger._log_queue.put(log)
 
     def _write_queue_to_file(self) -> None:
         """Function for the worker thread to write logs to file.
@@ -187,12 +186,12 @@ class Logger:
 
         try:
             with open(
-                self._log_file_path, mode="a", encoding="UTF-8"
+                Logger._log_file_path, mode="a", encoding="UTF-8"
             ) as log_file:
 
                 while True:
 
-                    log: Log = self._log_queue.get()
+                    log: Log = Logger._log_queue.get()
                     log_str: str = f"{log.date} {log.time} {log.message}\n"
 
                     # log to console
@@ -204,13 +203,13 @@ class Logger:
                     # lost if something happens
                     log_file.flush()
 
-                    self._log_queue.task_done()
+                    Logger._log_queue.task_done()
 
         except FileNotFoundError:
             # these errors are printed to console directly as logging obviously
             # does not work properly
             print(
-                f"ERROR: Could not find the log file {self._log_file_path}. "
+                f"ERROR: Could not find the log file {Logger._log_file_path}. "
                 "Logging will not work until this issue is fixed and the "
                 "server is restarted."
             )
@@ -221,7 +220,7 @@ class Logger:
             # these errors are printed to console directly as logging obviously
             # does not work properly
             print(
-                f"ERROR: While reading the log file {self._log_file_path}. "
+                f"ERROR: While reading the log file {Logger._log_file_path}. "
                 "Logging will not work until this issue is fixed and the "
                 "server is restarted."
             )
