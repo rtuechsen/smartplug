1ceda535e6a82fe89073e84560ea35f257f5327f
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-07-06 15:19:36 +0200

    use python again instead of python3

--- deploy_production_as_service.sh
+++ deploy_production_as_service.sh
@@ -28,11 +28,11 @@ cd backend
 
 printf "\n\n\x1B[33mStarting migration.\x1B[0m\n\n\n"
 
-sudo -E env PATH="$PATH" python3 manage.py clearsessions
+sudo -E env PATH="$PATH" python manage.py clearsessions
 
-sudo -E env PATH="$PATH" python3 manage.py makemigrations
+sudo -E env PATH="$PATH" python manage.py makemigrations
 
-sudo -E env PATH="$PATH" python3 manage.py migrate
+sudo -E env PATH="$PATH" python manage.py migrate
 
 printf "\n\n\x1B[33mMigration finished.\x1B[0m\n\n\n"
 
