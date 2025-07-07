b991afcb526d11404e3e41a3ab56b4df6e38d17d
Hannes <hannes.bethge@studen.fh-kiel.de>
2025-07-07 16:55:13 +0000

    Gunicorn now runs in SystemCTL when deploying in production.

--- deploy_production.sh
+++ deploy_production.sh
@@ -11,6 +11,11 @@ sudo cp ./mosquitto.conf /etc/mosquitto/mosquitto.conf
 
 sudo systemctl restart mosquitto
 
+sudo rm -r /etc/systemd/system/gunicorn.service
+sudo cp ./gunicorn.service /etc/systemd/system/gunicorn.service
+
+sudo systemctl enable gunicorn
+
 # frontend
 
 cd frontend
@@ -47,10 +52,9 @@ cd ..
 
 cd backend
 
-sudo systemctl start nginx
+sudo systemctl daemon-reload
 
-# TODO: adjust the number of workers or mention it in the admin documentation
-sudo -E env PATH="$PATH" gunicorn --bind 127.0.0.1:8000 smartplug_app.asgi:application --worker-class uvicorn.workers.UvicornWorker --workers 1 --graceful-timeout 0
+sudo systemctl restart nginx
 
 sudo systemctl restart gunicorn
 
