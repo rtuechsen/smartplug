78c9228c94eb7b96a5fb4e1662ebf7ea4f1bc2cf
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-07-10 11:01:54 +0200

    allow writing logs to everyone

--- change_permissions.sh
+++ change_permissions.sh
@@ -37,14 +37,16 @@ sudo chmod -R 645 /etc/mosquitto
 
 # logs
 
+# 655
+
 sudo chown -R root:root /var/log/nginx/
-sudo chmod -R 665 /var/log/nginx/
+sudo chmod -R 777 /var/log/nginx/
 
 sudo chown -R root:root /var/log/mosquitto/
-sudo chmod -R 665 /var/log/mosquitto/
+sudo chmod -R 777 /var/log/mosquitto/
 
 sudo chown -R root:root /var/log/smartplug_app/
-sudo chmod -R 665 /var/log/smartplug_app/
+sudo chmod -R 777 /var/log/smartplug_app/
 
 
 
