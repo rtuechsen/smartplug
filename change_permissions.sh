faa0a8b9e11a70e12a58820f604128e6ee9fc303
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-07-11 19:23:51 +0200

    change permission

--- change_permissions.sh
+++ change_permissions.sh
@@ -24,8 +24,8 @@ sudo chmod -R 645 /etc/mosquitto
 sudo chown root:www-data /etc/mosquitto/mosquitto_passwd.json
 sudo chmod 660 /etc/mosquitto/mosquitto_passwd.json		# other users = 0 so that they cannot read the secret
 
-sudo chown mosquitto: /etc/mosquitto/passwd
-sudo chmod 600 /etc/mosquitto/passwd					# other users = 0 so that they cannot read the secret
+sudo chown root:mosquitto /etc/mosquitto/passwd
+sudo chmod 660 /etc/mosquitto/passwd					# other users = 0 so that they cannot read the secret
 
 # logs
 
