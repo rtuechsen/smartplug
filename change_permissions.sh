9c4d635c9bc641ff78771c3a0ee2284aa256d72c
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-07-11 10:06:46 +0200

    added comments

--- change_permissions.sh
+++ change_permissions.sh
@@ -1,13 +1,15 @@
 
-# TODO: add explanation
+# This script changes the permissions of files and directories relevant to the smartplug-dirigent
+# such that the default user of the ubuntu system cannot modify or delete them without providing
+# a password. The permissions have to be low enough to allow the system to work.
 
 # nginx
 
 sudo chown -R root:root /etc/nginx/
-sudo chmod -R 645 /etc/nginx/
+sudo chmod -R 645 /etc/nginx/							# other users = 5 needed to allow the 'cd' command to work
 
 sudo chown -R root:root /etc/nginx/ssl/
-sudo chmod -R 640 /etc/nginx/ssl/
+sudo chmod -R 640 /etc/nginx/ssl/						# other users = 0 so that they cannot read the secret
 
 # service
 
@@ -20,10 +22,10 @@ sudo chown -R root:root /etc/mosquitto
 sudo chmod -R 645 /etc/mosquitto
 
 sudo chown root:www-data /etc/mosquitto/mosquitto_passwd.json
-sudo chmod 660 /etc/mosquitto/mosquitto_passwd.json
+sudo chmod 660 /etc/mosquitto/mosquitto_passwd.json		# other users = 0 so that they cannot read the secret
 
 sudo chown mosquitto: /etc/mosquitto/passwd
-sudo chmod 600 /etc/mosquitto/passwd
+sudo chmod 600 /etc/mosquitto/passwd					# other users = 0 so that they cannot read the secret
 
 # logs
 
