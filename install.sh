d8717246fb73bd833d2ba08b2b269a1befa0caa4
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-07-12 12:04:08 +0200

    fix path

--- install.sh
+++ install.sh
@@ -48,7 +48,7 @@ sudo mosquitto_passwd -c /etc/mosquitto/passwd mqttuser
 if ! [ -d /etc/django/ ]; then
 	sudo mkdir /etc/django/
 fi
-sudo -E env PATH="$PATH" python ./backend/smartplug_app/generate_secrets.py
+sudo -E env PATH="$PATH" python ./smartplug_app/generate_secrets.py
 
 cd ..
 
