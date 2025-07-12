6de25f198675069388f7854014840d1896c7660e
Robert Tüchsen <robert.tuechsen@student.fh-kiel.de>
2025-07-12 11:49:38 +0200

    generate secret keys

--- backend/smartplug_app/generate_secrets.py
+++ backend/smartplug_app/generate_secrets.py
@@ -2,7 +2,8 @@
 
 import secrets
 
-with open(
-    "/etc/django_secret_key_development.txt", "w", encoding="UTF-8"
-) as f:
+with open("/etc/django/secret_key_development", "w", encoding="UTF-8") as f:
+    f.write(secrets.token_urlsafe(32))
+
+with open("/etc/django/secret_key_production", "w", encoding="UTF-8") as f:
     f.write(secrets.token_urlsafe(32))
--- install.sh
+++ install.sh
@@ -45,6 +45,8 @@ sudo systemctl stop mosquitto
 sudo mosquitto_passwd -c /etc/mosquitto/passwd mqttuser
 
 # TODO: call generate_secrets.py to generate secrets for django for development and production
+sudo mkdir /etc/django/
+sudo python ./backend/smartplug_app/generate_secrets.py
 
 cd ..
 
